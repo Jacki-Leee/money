4b9e0ed0837e7ef27e167e0ed48438f5f4914c75
coneseo <woon4754@gmail.com>
2019-06-14 15:58:06 +0900

    [추가] 가져온 환율 정보 중 송금국가와 수취국가에 따른 환율 가져오기 (Controller에 getSendAmount 메소드 구현)

--- src/main/java/com/example/currencyconverter/controller/CurrencyConverterAPIController.java
+++ src/main/java/com/example/currencyconverter/controller/CurrencyConverterAPIController.java
@@ -1,6 +1,6 @@
 package com.example.currencyconverter.controller;
 
-import com.example.currencyconverter.dto.ConvertDto;
+import com.example.currencyconverter.dto.ConvertInfoDto;
 import com.example.currencyconverter.service.CurrencyConverterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.DecimalFormat;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -17,9 +18,20 @@ public class CurrencyConverterAPIController {
     private final DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
 
     @GetMapping("/exchange-rates")
-    public ResponseEntity getExchangeRate(@RequestBody ConvertDto convertDto){
-        Double exchangeRate = currencyConverter.getCurrencyRate(convertDto.getSendCountry(), convertDto.getReceiveCountry());
+    public ResponseEntity getExchangeRate(@RequestBody ConvertInfoDto convertInfo){
+        Double exchangeRate = currencyConverter.getCurrencyRate(convertInfo.getSendCountry(), convertInfo.getReceiveCountry());
         return new ResponseEntity(format(exchangeRate), HttpStatus.OK);
+    }
+
+    @GetMapping("/send-amounts")
+    public ResponseEntity getSendAmount(@RequestBody  ConvertInfoDto convertInfo){
+
+        Double currency = currencyConverter.getCurrencyRate(convertInfo.getSendCountry(), convertInfo.getReceiveCountry());
+        Double sendAmount = (currency * convertInfo.getSendAmount());
+        String formatSendAmount = format(sendAmount);
+        
+
+        return new ResponseEntity(formatSendAmount, HttpStatus.OK);
     }
 
     public String format(Number number){
