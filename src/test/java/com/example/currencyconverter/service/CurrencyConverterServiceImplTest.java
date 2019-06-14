6d8c50edb03fe8d7224243ed46f4de5995e1ca11
coneseo <woon4754@gmail.com>
2019-06-14 16:22:49 +0900

    [추가] 가져온 환율 정보 중 송금국가와 수취국가에 따른 환율 가져오기 (테스트 코드 작성 - 직접 API를 호출해서 가져온 값과 service메소드를 통해 가져온 값 비교)

---
+++ src/test/java/com/example/currencyconverter/service/CurrencyConverterServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.example.currencyconverter.service;
+
+import com.example.currencyconverter.dto.CurrencyDto;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyConverterServiceImplTest {
+
+    @Autowired
+    CurrencyAPIService currencyAPIService;
+    @Autowired
+    CurrencyConverterService currencyConverterService;
+
+    @Test
+    public void 송금액_잘_가져왔는지_확인() {
+        CurrencyDto currencies = currencyAPIService.getCurrency();
+        Double currencyFromDirectAPI = currencies.getQuotes().get("USDPHP");
+        Double currencyFromConverterService = currencyConverterService.getCurrencyRate("USD", "PHP");
+        Assert.assertEquals(currencyFromDirectAPI, currencyFromConverterService);
+    }
+}
